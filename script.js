6f651e034bfbfd353abf95ef12c50dfac9f8fd2a
Akashx2606 <137022133+Akashx2606@users.noreply.github.com>
2025-05-03 15:14:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function decimalToBinary(num) {
   //Write you code here
+	let binery = '';
+	if(num === 0){
+			return '0';
+	}
+	for(let i=num;i>0;i=math.floor(i/2)){
+			binery = (i%2)  +  binery;
+	}
+	return binery;
   
 }
 
